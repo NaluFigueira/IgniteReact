d6883a4e11a4070b6d890d55d2e717cf66129578
NaluFigueira <anachfigueira@gmail.com>
2021-07-30 16:32:26 -0300

    Initializing project

---
+++ 02-shopping-cart/src/hooks/useCart.tsx
@@ -0,0 +1,87 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../services/api';
+import { Product, Stock } from '../types';
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+interface UpdateProductAmount {
+  productId: number;
+  amount: number;
+}
+
+interface CartContextData {
+  cart: Product[];
+  addProduct: (productId: number) => Promise<void>;
+  removeProduct: (productId: number) => void;
+  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+  getCartSize: () => number;
+}
+
+const CartContext = createContext<CartContextData>({} as CartContextData);
+
+export function CartProvider({ children }: CartProviderProps): JSX.Element {
+  const [cart, setCart] = useState<Product[]>(() => {
+    // const storagedCart = Buscar dados do localStorage
+
+    // if (storagedCart) {
+    //   return JSON.parse(storagedCart);
+    // }
+
+    return [];
+  });
+
+  const addProduct = async (productId: number) => {
+    try {
+      const { data } = await api.get(`products/${productId}`);
+      setCart([...cart, data]);
+    } catch {
+      // TODO
+    }
+  };
+
+  const removeProduct = (productId: number) => {
+    try {
+      // TODO
+    } catch {
+      // TODO
+    }
+  };
+
+  const updateProductAmount = async ({
+    productId,
+    amount,
+  }: UpdateProductAmount) => {
+    try {
+      // TODO
+    } catch {
+      // TODO
+    }
+  };
+
+  const getCartSize = ():number => {
+    let productsInCart: number[] = [];
+    cart.forEach((product) => {
+      if(!productsInCart.includes(product.id)) {
+        productsInCart.push(product.id);
+      }
+    });
+    return productsInCart.length;
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addProduct, removeProduct, updateProductAmount, getCartSize }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart(): CartContextData {
+  const context = useContext(CartContext);
+
+  return context;
+}
