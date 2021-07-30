a0116697210e444d3c8b498713474cdac6b3496e
NaluFigueira <anachfigueira@gmail.com>
2021-07-30 15:14:14 -0300

    Creating genres context

--- 02-movies-list/src/components/SideBar.tsx
+++ 02-movies-list/src/components/SideBar.tsx
@@ -1,3 +1,28 @@
+import { useGenres } from "../hooks/useGenres";
+import { Button } from '../components/Button';
+
 export function SideBar() {
-  // Complete aqui
+  const {genres, selectedGenreId, updateSelectedGenreId} = useGenres();
+
+  function handleClickButton(id: number) {
+    updateSelectedGenreId(id);
+  }
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
