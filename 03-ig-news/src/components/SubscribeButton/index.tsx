7ed4500dead99633428cb7ab5d53d2eb43ee2c01
NaluFigueira <anachfigueira@gmail.com>
2021-08-22 03:16:53 -0300

    Generating session with stripe

--- 03-ig-news/src/components/SubscribeButton/index.tsx
+++ 03-ig-news/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripejs';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +8,34 @@ interface SubscribeButtonProps {
 }
 
 const SubscribeButton: React.FC<SubscribeButtonProps> = ({ priceId }) => {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+
+    } catch(err) {
+      alert(err.message)
+    }
+  }
+
   return (
-      <button type="button" className={styles.subscribeButton}>
+      <button 
+        type="button" 
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}
+      >
           Subscribe now
       </button>
   );
