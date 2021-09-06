cd7fa305d1cbf19ec17815fddfb55b5cc7a07e05
NaluFigueira <anachfigueira@gmail.com>
2021-09-06 17:58:23 -0300

    Creating home page

--- 03-space-traveling/src/pages/_document.tsx
+++ 03-space-traveling/src/pages/_document.tsx
@@ -1,7 +1,19 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
 }
--- 03-space-traveling/src/pages/index.tsx
+++ 03-space-traveling/src/pages/index.tsx
@@ -1,12 +1,16 @@
-import { GetStaticProps } from 'next';
+import { useState } from 'react';
+import { GetStaticPropsResult } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import Prismic from '@prismicio/client';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
 import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { formatReceivedDateString } from '../utils/date';
 
 interface Post {
   uid?: string;
@@ -27,25 +31,71 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-const Home: React.FC = () => {
-  const { asPath } = useRouter();
+const formatResponseResult = (results: Post[]): Post[] => {
+  return results.map(post => ({
+    uid: post.uid,
+    first_publication_date: formatReceivedDateString(
+      post.first_publication_date
+    ),
+    data: post.data,
+  }));
+};
+
+const Home: React.FC<HomeProps> = ({ postsPagination }) => {
+  const { results } = postsPagination;
+  const formattedResults = formatResponseResult(results);
+  const [currentPath, setCurrentPath] = useState('/');
 
   return (
-    <>
+    <div className={commonStyles.mainContainer}>
       <Head>
         <title>Home | Space Traveling</title>
       </Head>
 
-      <Header currentPath={asPath} />
-    </>
+      <Header currentPath={currentPath} />
+
+      {formattedResults.map(post => (
+        <div className={styles.postContainer} key={post.uid}>
+          <span className={styles.postTitle}>{post.data.title}</span>
+          <span className={styles.postSubtitle}>{post.data.subtitle}</span>
+          <div>
+            <div>
+              <FiCalendar />
+              <span>{post.first_publication_date}</span>
+            </div>
+            <div>
+              <FiUser />
+              <span>{post.data.author}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
 export default Home;
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps = async (): Promise<
+  GetStaticPropsResult<HomeProps>
+> => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author', 'posts.content'],
+      pageSize: 5,
+    }
+  );
 
-//   // TODO
-// };
+  const { next_page, results } = postsResponse;
+
+  return {
+    props: {
+      postsPagination: {
+        next_page,
+        results,
+      },
+    },
+  };
+};
