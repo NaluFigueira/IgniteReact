c9b6c11b15283eedabc74b6c8c5fd08e0316d1cc
NaluFigueira <anachfigueira@gmail.com>
2021-09-10 16:48:36 -0300

    Adding a load more button

--- 03-space-traveling/src/pages/index.tsx
+++ 03-space-traveling/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { GetStaticPropsResult } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
 
 import Header from '../components/Header';
 
@@ -23,7 +24,7 @@ interface Post {
 }
 
 interface PostPagination {
-  next_page: string;
+  next_page: string | null;
   results: Post[];
 }
 
@@ -42,9 +43,22 @@ const formatResponseResult = (results: Post[]): Post[] => {
 };
 
 const Home: React.FC<HomeProps> = ({ postsPagination }) => {
-  const { results } = postsPagination;
-  const formattedResults = formatResponseResult(results);
+  const formattedResults = formatResponseResult(postsPagination.results);
+  const [posts, setPosts] = useState<Post[]>(formattedResults);
+  const [nextPageUrl, setNextPageUrl] = useState<string | null>(
+    postsPagination.next_page
+  );
   const [currentPath, setCurrentPath] = useState('/');
+
+  const getNextPagePosts = (): void => {
+    fetch(nextPageUrl)
+      .then(T => T.json())
+      .then((response: ApiSearchResponse) => {
+        const newPosts = formatResponseResult(response.results);
+        setPosts([...posts, ...newPosts]);
+        setNextPageUrl(response.next_page);
+      });
+  };
 
   return (
     <div className={commonStyles.mainContainer}>
@@ -54,7 +68,7 @@ const Home: React.FC<HomeProps> = ({ postsPagination }) => {
 
       <Header currentPath={currentPath} />
 
-      {formattedResults.map(post => (
+      {posts.map(post => (
         <div className={styles.postContainer} key={post.uid}>
           <span className={styles.postTitle}>{post.data.title}</span>
           <span className={styles.postSubtitle}>{post.data.subtitle}</span>
@@ -70,6 +84,16 @@ const Home: React.FC<HomeProps> = ({ postsPagination }) => {
           </div>
         </div>
       ))}
+
+      {nextPageUrl && (
+        <button
+          className={styles.loadMoreButton}
+          onClick={getNextPagePosts}
+          type="button"
+        >
+          Carregar mais posts
+        </button>
+      )}
     </div>
   );
 };
@@ -84,7 +108,9 @@ export const getStaticProps = async (): Promise<
     [Prismic.predicates.at('document.type', 'posts')],
     {
       fetch: ['posts.title', 'posts.subtitle', 'posts.author', 'posts.content'],
-      pageSize: 5,
+      pageSize: 2,
+      page: 1,
+      orderings: '[document.last_publication_date desc]',
     }
   );
 
